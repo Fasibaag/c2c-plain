5068aa91838cabcffee83ae4194fb569a40be069
Fasibaag <fasibaag@gmail.com>
2019-04-21 20:52:00 -0700

    UI Home page

---
+++ main.js
@@ -0,0 +1,11 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:false,
+    dots:true,
+    lazyLoad:true,
+    items:1,
+    autoplay: true,
+    smartSpeed: 1000,
+    // animateOut: "fadeOut"
+})
