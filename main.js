55594b9ad8de674e1880fdd770f61b66552165a4
Fasibaag <fasibaag@gmail.com>
2019-07-22 00:41:31 -0700

    side bar impmented

--- main.js
+++ main.js
@@ -1,11 +1,42 @@
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:0,
-    nav:false,
-    dots:true,
-    lazyLoad:true,
-    items:1,
+    loop: true,
+    margin: 0,
+    nav: false,
+    dots: true,
+    lazyLoad: true,
+    items: 1,
     autoplay: true,
     smartSpeed: 1000,
     // animateOut: "fadeOut"
 })
+
+
+$('.menu').click(function () {
+    var menuList = document.querySelector('.sideBar-menu');
+    var body = document.querySelector('body');
+    if (menuList.style.display !== 'block') {
+        menuList.style.display = 'block';
+    }
+
+    $(".menu-list-mob").animate({
+        width: "250px"
+    });
+
+    body.classList.add('overflow');
+})
+
+$('.closeIcon').click(function () {
+    var menuList = document.querySelector('.sideBar-menu');
+    var body = document.querySelector('body');
+    $( ".menu-list-mob" ).animate({
+        width: "0px"
+      }, function() {
+        if (menuList.style.display !== 'none') {
+            menuList.style.display = 'none';
+        }
+      });
+
+
+
+    body.classList.remove('overflow');
+});
